130767386029ffc11c9c423a062564b668853621
hjh830827 <hjh830827@gmail.com>
2022-05-01 21:29:26 +0900

    checkBox test

--- src/views/CheckBoxAllSelect.vue
+++ src/views/CheckBoxAllSelect.vue
@@ -11,7 +11,7 @@
         v-model="selectList"
         :key="index"
       />
-      <label :for="item" :key="index"> {{ item }}</label>
+      <label :for="item" :key="index + '1'"> {{ item }}</label>
     </template>
     <br />
     <span>check: {{ selectList }}</span>
